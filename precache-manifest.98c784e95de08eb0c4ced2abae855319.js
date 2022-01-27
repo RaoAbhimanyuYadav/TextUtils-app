e386a0a9919e3e31983ebcc1eef114a7a6427d29
RaoAbhimanyuYadav <raoyadavabhi@gmail.com>
2022-01-27 13:55:07 +0530

    Updates

---
+++ precache-manifest.98c784e95de08eb0c4ced2abae855319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "747c195bb0eaae52d4258cc66c84c1ec",
+    "url": "/TextUtils-app/index.html"
+  },
+  {
+    "revision": "29d1acf8971ba3d3d2b7",
+    "url": "/TextUtils-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/TextUtils-app/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "bd81e5c6feaf63e9a711",
+    "url": "/TextUtils-app/static/js/3.4183067e.chunk.js"
+  },
+  {
+    "revision": "29d1acf8971ba3d3d2b7",
+    "url": "/TextUtils-app/static/js/main.55b1ea09.chunk.js"
+  },
+  {
+    "revision": "ee709a07584b2066f5dd",
+    "url": "/TextUtils-app/static/js/runtime~main.cde59786.js"
+  }
+]);
